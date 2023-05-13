d0532e16dd200dd3a9ff26ef771ecd4c19517750
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-05-13 16:07:34 +0000

    Create the homepage

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+
+const Home = () => {
+  return (
+    <div className="home-section position-relative row w-100 m-0">
+      <div className="col-lg-4 col-md-5 col-sm-12 col-12">
+      </div>
+      <div className="col-lg-8 col-md-7 col-sm-12 col-12">
+      </div>
+    </div>
+  );
+};
+
+export default Home;
