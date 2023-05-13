d7d2caf24b33b9bdefa75238824d49540bba91f1
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-05-13 16:05:32 +0000

    Set user theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState<string | null>('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const setInitialTheme = () => {
+    const root = document.querySelector(':root');
+    root?.setAttribute(
+      'color-scheme',
+      `${
+        localStorage.getItem('app-theme')
+          ? localStorage.getItem('app-theme')
+          : theme
+      }`
+    );
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem('app-theme')) {
+      setTheme(localStorage.getItem('app-theme'));
+    } else {
+      const currentTheme = window.matchMedia('(prefers-color-scheme: light)')
+        .matches
+        ? 'light'
+        : 'dark';
+      localStorage.setItem('app-theme', currentTheme);
+      setTheme(currentTheme);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (theme) {
+      setInitialTheme();
+    }
+  }, [theme]);
+
+  return <div className="App"></div>;
 }
 
-export default App
+export default App;
